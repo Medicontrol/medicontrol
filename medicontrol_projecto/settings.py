63faddc8c2dcbf21d087bda6816b0145cf4b3bf5
danielurueta937-a11y <danielurueta937@gmail.com>
2026-05-20 10:14:54 -0500

    Configurar PostgreSQL para despliegue en Render

--- medicontrol_projecto/settings.py
+++ medicontrol_projecto/settings.py
@@ -14,6 +14,8 @@ Django settings for medicontrol_projecto project.
 
 import os
 from pathlib import Path
+import dj_database_url
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,11 +75,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'medicontrol_projecto.wsgi.application'
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
